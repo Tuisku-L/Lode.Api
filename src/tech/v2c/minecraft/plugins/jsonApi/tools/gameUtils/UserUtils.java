ff49199436518973fc42083ac33eec68d3d5b9f4
Tuisku <tuisku.wood@lode.li>
2019-08-17 23:15:05 +0800

    refactor: 修改 UserUtils 获取 Server 对象方式.

--- src/tech/v2c/minecraft/plugins/jsonApi/tools/gameUtils/UserUtils.java
+++ src/tech/v2c/minecraft/plugins/jsonApi/tools/gameUtils/UserUtils.java
@@ -2,13 +2,14 @@ package tech.v2c.minecraft.plugins.jsonApi.tools.gameUtils;
 
 import cn.nukkit.Player;
 import cn.nukkit.Server;
+import tech.v2c.minecraft.plugins.jsonApi.JsonApi;
 
 import java.util.Map;
 import java.util.UUID;
 
 public class UserUtils {
-    public static Player GetPlayerByName(Server server, String name){
-        Map<UUID, Player> onlinePlayers = server.getOnlinePlayers();
+    public static Player GetPlayerByName(String name){
+        Map<UUID, Player> onlinePlayers = JsonApi.instance.getServer().getOnlinePlayers();
         Player player = null;
         for (Map.Entry<UUID, Player> item : onlinePlayers.entrySet()) {
             if(item.getValue().getName().equalsIgnoreCase(name)){
