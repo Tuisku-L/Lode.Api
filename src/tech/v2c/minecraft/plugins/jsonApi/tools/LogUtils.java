bf6ec479ba5926af6b9dd8ab9892d996392baea2
Tuisku <tuiskuwood@outlook.com>
2019-08-23 19:09:29 +0800

    fix: 修复 全局日志输出类空指针

--- src/tech/v2c/minecraft/plugins/jsonApi/tools/LogUtils.java
+++ src/tech/v2c/minecraft/plugins/jsonApi/tools/LogUtils.java
@@ -1,28 +1,20 @@
 package tech.v2c.minecraft.plugins.jsonApi.tools;
 
-import cn.nukkit.Server;
 import cn.nukkit.utils.LogLevel;
-import cn.nukkit.utils.MainLogger;
 import tech.v2c.minecraft.plugins.jsonApi.JsonApi;
 
 public class LogUtils {
-    private static MainLogger logger;
-
-    public LogUtils(){
-        this.logger = JsonApi.instance.getServer().getLogger();
-    }
-
     public static void Log(LogLevel level, String msg){
-        logger.log(level, msg);
+        JsonApi.instance.getLogger().log(level, msg);
     }
 
     public static void Debug(String msg){
         if(JsonApi.instance.isDebugMode){
-            logger.debug(msg);
+            JsonApi.instance.getLogger().debug(msg);
         }
     }
 
     public static void Info(String msg){
-        logger.info(msg);
+        JsonApi.instance.getLogger().info(msg);
     }
 }
