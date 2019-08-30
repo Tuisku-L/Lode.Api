0888f2dd4ed7edab66b2bc390e62dc16b8428077
Tuisku <tuisku.wood@lode.li>
2019-08-31 03:52:01 +0800

    feat: 添加 WebSocket 服务器鉴权登录功能.

--- src/tech/v2c/minecraft/plugins/jsonApi/EventNotify/global/JsonApiWebSocketServer.java
+++ src/tech/v2c/minecraft/plugins/jsonApi/EventNotify/global/JsonApiWebSocketServer.java
@@ -1,14 +1,14 @@
 package tech.v2c.minecraft.plugins.jsonApi.EventNotify.global;
 
 import cn.nukkit.command.ConsoleCommandSender;
+import cn.nukkit.utils.ConfigSection;
+import com.google.gson.Gson;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 import tech.v2c.minecraft.plugins.jsonApi.JsonApi;
 import tech.v2c.minecraft.plugins.jsonApi.tools.results.JsonResult;
 
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 
@@ -16,11 +16,11 @@ public class JsonApiWebSocketServer extends WebSocketServer {
     public static HashMap<Object, WebSocket> connPool = new HashMap<Object, WebSocket>();
     public static final boolean executeByWs = JsonApi.instance.getConfig().getSection("EventListener").getBoolean("ExecuteByWs");
 
-    public JsonApiWebSocketServer(){
+    public JsonApiWebSocketServer() {
         super(new InetSocketAddress(JsonApi.instance.getConfig().getSection("Server").getString("IP"), JsonApi.instance.getConfig().getSection("Server").getInt("WsPort")));
     }
 
-    public static void SendMsg(JsonResult result){
+    public static void SendMsg(JsonResult result) {
         JsonApiWebSocketServer.connPool.values().forEach(conn -> {
             conn.send(result);
         });
@@ -28,7 +28,6 @@ public class JsonApiWebSocketServer extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
-        connPool.put(conn.getDraft(), conn);
     }
 
     @Override
@@ -38,13 +37,65 @@ public class JsonApiWebSocketServer extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket conn, String message) {
-        if(executeByWs){
-            JsonApi.instance.getServer().dispatchCommand(new ConsoleCommandSender(), message);
+        System.out.println(message);
+        Gson gson = new Gson();
+        WsMessage msg = gson.fromJson(message, WsMessage.class);
+        if (msg.getAction().equalsIgnoreCase("auth")) {
+            ConfigSection authConf = JsonApi.instance.getConfig().getSection("Server").getSection("Authentication");
+            String userName = authConf.getString("UserName");
+            String password = authConf.getString("Password");
+
+            HashMap<String, Object> authInfo = msg.getParams();
+            if (userName.equals(authInfo.get("userName")) && password.equals(authInfo.get("password"))) {
+                if (!connPool.containsKey(conn.getDraft())) {
+                    connPool.put(conn.getDraft(), conn);
+                    conn.send(gson.toJson(new JsonResult(null, 204, "auth success!")));
+                } else {
+                    conn.send(gson.toJson(new JsonResult(null, 204, "already login.")));
+                }
+            } else {
+                conn.send(gson.toJson(new JsonResult(null, 401, "auth failed!")));
+            }
         }
+
+        if (msg.getAction().equalsIgnoreCase("executeCmd")) {
+            if (connPool.containsKey(conn.getDraft())) {
+                if (executeByWs) {
+                    JsonApi.instance.getServer().dispatchCommand(new ConsoleCommandSender(), msg.getParams().get("command").toString());
+                    conn.send(gson.toJson(new JsonResult(null, 204, "execute success.")));
+                }
+            } else {
+                conn.send(gson.toJson(new JsonResult(null, 401, "need login!")));
+            }
+        }
+
+        conn.send(gson.toJson(new JsonResult(null,403,"unknown command.")));
     }
 
     @Override
     public void onError(WebSocket conn, Exception ex) {
+        Gson gson = new Gson();
+        conn.send(gson.toJson(new JsonResult("unknown command.", 403, ex.getMessage())));
+    }
 
+    public class WsMessage {
+        private String action;
+        private HashMap<String, Object> params;
+
+        public String getAction() {
+            return action;
+        }
+
+        public void setAction(String action) {
+            this.action = action;
+        }
+
+        public HashMap<String, Object> getParams() {
+            return params;
+        }
+
+        public void setMessage(HashMap<String, Object> params) {
+            this.params = params;
+        }
     }
 }
