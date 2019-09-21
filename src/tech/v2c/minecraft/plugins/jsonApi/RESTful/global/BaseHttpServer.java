bc63383f3fabc5ec08a2868697dcf7ecfc21a611
Tuisku <tuiskuwood@outlook.com>
2019-09-22 04:08:06 +0800

    fix: 修复 判断鉴权条件错误的 Bug.

--- src/tech/v2c/minecraft/plugins/jsonApi/RESTful/global/BaseHttpServer.java
+++ src/tech/v2c/minecraft/plugins/jsonApi/RESTful/global/BaseHttpServer.java
@@ -1,19 +1,17 @@
 package tech.v2c.minecraft.plugins.jsonApi.RESTful.global;
 
-import cn.nukkit.utils.Config;
 import cn.nukkit.utils.ConfigSection;
 import com.google.gson.Gson;
+import org.bukkit.configuration.ConfigurationSection;
 import org.nanohttpd.protocols.http.IHTTPSession;
 import org.nanohttpd.protocols.http.NanoHTTPD;
 import org.nanohttpd.protocols.http.response.Response;
 import org.nanohttpd.protocols.http.response.Status;
 import tech.v2c.minecraft.plugins.jsonApi.JsonApi;
-import tech.v2c.minecraft.plugins.jsonApi.RESTful.global.entities.JsonData;
 import tech.v2c.minecraft.plugins.jsonApi.tools.EncryptUtils;
 
 import java.io.*;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class BaseHttpServer extends NanoHTTPD {
@@ -28,32 +26,62 @@ public class BaseHttpServer extends NanoHTTPD {
     public Response serve(IHTTPSession session) {
         String uri = session.getUri();
         if (!JsonApi.instance.isDebugMode) {
-            String clientAuthStr = session.getHeaders().get("x-jsonapi-authentication");
-            if (clientAuthStr == null) {
-                return Response.newFixedLengthResponse(Status.UNAUTHORIZED, MIME_PLAINTEXT, "Error:  401 Unauthorized. Need Authentication.");
-            }
+            if(!uri.toLowerCase().contains("/api/server/getstatus")){
+                String clientAuthStr = session.getHeaders().get("x-lode-authentication");
+                // 没有鉴权头
+                if (clientAuthStr == null || clientAuthStr.equals("")) {
+                    return Response.newFixedLengthResponse(Status.UNAUTHORIZED, MIME_PLAINTEXT, "Error:  401 Unauthorized. 需要鉴权信息.");
+                }
 
-            String serverAuthStr = GetAuthentication(uri);
-            if (!serverAuthStr.equalsIgnoreCase(clientAuthStr)) {
-                return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 403 Forbidden. Authentication failed.");
+                //鉴权头不完整
+                if (clientAuthStr.split(";").length != 2) {
+                    return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 403 Forbidden. 鉴权信息不完整.");
+                }
+
+                // 鉴权信息格式:
+                // token: {token}; ts: {timestamp}
+                try {
+                    long ts = Long.parseLong(clientAuthStr.split(";")[1].split("=")[1].trim());
+                    if (!CheckTimestamp(ts)) {
+                        return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 403 Forbidden. Timeout.");
+                    }
+
+                    String serverAuthStr = GetAuthentication(uri);
+                    String token = clientAuthStr.split(";")[0].split("=")[1].trim();
+                    if (!serverAuthStr.equalsIgnoreCase(token)) {
+                        return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 403 Forbidden. Authentication failed.");
+                    }
+                } catch (Exception e) {
+                    return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 403 Forbidden. 鉴权信息不正确. " + e.getMessage());
+                }
             }
         }
 
-        Map<String, List<String>> parameters = session.getParameters();
-        JsonData jsonData = new Gson().fromJson(GetQueryString(parameters, "Data"), JsonData.class);
-
-        if(!JsonApi.instance.isDebugMode){
-            if (!CheckTimestamp(jsonData.TimeStamp)) {
-                return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 403 Forbidden. Timeout.");
-            }
+        // 从 InputStream 获取 Body
+        String body;
+        try {
+            body = GetBody(session.getInputStream());
+        } catch (IOException e) {
+            return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 500 INTERNAL ERROR. 请求体不正确. " + e.getMessage());
         }
 
+        // Body => Map
+        Map<String, Object> jsonData;
+        try {
+            jsonData = new Gson().fromJson(body, Map.class);
+        } catch (Exception e) {
+            return Response.newFixedLengthResponse(Status.FORBIDDEN, MIME_PLAINTEXT, "Error: 500 INTERNAL ERROR. 请求体格式不正确. " + e.getMessage());
+        }
+
+        // 如果是特定的文件上传请求, 把文件放进 Map
         if (uri.toLowerCase().contains("upload") || uri.toLowerCase().contains("install")) {
+            jsonData = new HashMap<>();
             Map<String, File> allFile = UploadFiles(session);
-            jsonData.Data.put("files", allFile);
+           jsonData.put("files", allFile);
         }
 
-        String result = "";
+        // 调用指定的 action
+        String result;
         try {
             result = RouteManage.TouchAction(uri.toLowerCase(), jsonData);
         } catch (Exception e) {
@@ -67,13 +95,6 @@ public class BaseHttpServer extends NanoHTTPD {
         return Response.newFixedLengthResponse(Status.OK, "application/json; charset=utf-8", result);
     }
 
-    private String GetQueryString(Map<String, List<String>> parameters, String key) {
-        if (parameters.containsKey(key)) {
-            return parameters.get(key).get(0);
-        }
-        return null;
-    }
-
     private Map<String, File> UploadFiles(IHTTPSession session) {
         Map<String, String> files = new HashMap<>();
         try {
@@ -84,7 +105,7 @@ public class BaseHttpServer extends NanoHTTPD {
             e.printStackTrace();
         }
 
-        Map<String, File> allFile = new HashMap<String, File>();
+        Map<String, File> allFile = new HashMap<>();
 
         for (String key : session.getParameters().keySet()) {
             if (key.equalsIgnoreCase("data")) continue;
@@ -106,5 +127,14 @@ public class BaseHttpServer extends NanoHTTPD {
 
     private boolean CheckTimestamp(long ts) {
         return (Math.abs((System.currentTimeMillis() - ts)) / (1000 * 60)) <= 2;
+    }
+
+    private String GetBody(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        int count = inputStream.available();
+        byte[] buffer = new byte[count];
+        result.write(buffer, 0, inputStream.read(buffer));
+
+        return result.toString("UTF-8");
     }
 }
