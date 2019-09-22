d670c43c7f3b3ddcbdbda9cb52a03f8d5c29c542
Tuisku <tuiskuwood@outlook.com>
2019-09-22 19:59:35 +0800

    feat: 添加 WebSocket 功能在调试模式下无需鉴权.

--- src/tech/v2c/minecraft/plugins/lode/EventNotify/global/LodeWebSocketServer.java
+++ src/tech/v2c/minecraft/plugins/lode/EventNotify/global/LodeWebSocketServer.java
@@ -28,6 +28,11 @@ public class LodeWebSocketServer extends WebSocketServer {
 
     @Override
     public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        if(Lode.instance.isDebugMode){
+            connPool.put(conn.getDraft(), conn);
+            Gson gson = new Gson();
+            conn.send(gson.toJson(new JsonResult(null, 204, "Debug mode.")));
+        }
     }
 
     @Override
@@ -60,7 +65,7 @@ public class LodeWebSocketServer extends WebSocketServer {
         }
 
         if (msg.getAction().equalsIgnoreCase("executeCmd")) {
-            if (connPool.containsKey(conn.getDraft())) {
+            if (Lode.instance.isDebugMode || connPool.containsKey(conn.getDraft())) {
                 if (executeByWs) {
                     Lode.instance.getServer().getScheduler().scheduleTask(Lode.instance, new Runnable() {
                         @Override
